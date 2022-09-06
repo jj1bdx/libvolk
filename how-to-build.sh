0ce63fb78c31def041cdc4368f76ab7013198848
Kenji Rikitake <kenji.rikitake@acm.org>
2022-09-06 15:50:52 +0000

    Revise how-to-build.sh with installation commands

--- how-to-build.sh
+++ how-to-build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-# cmake script for macOS
+# cmake script for macOS and Ubuntu
 cmake -S . -B build -DBUILD_TESTING=OFF -DENABLE_TESTING=OFF -DVOLK_CPU_FEATURES=ON -DVOLK_USE_OWN_CPU_FEATURES=ON
 MAKEFLAGS="-j12" cmake --build build
+# after building, do:
+# sudo zsh
+# umask 022
+# cmake --install build
