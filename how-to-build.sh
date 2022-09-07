4b900266b3055d084bc0d8dcd79584a39e521537
Kenji Rikitake <kenji.rikitake@acm.org>
2022-09-07 12:00:54 +0900

    Add M1 Mac build info

--- how-to-build.sh
+++ how-to-build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-# cmake script for macOS and Ubuntu
+# cmake script for macOS and Ubuntu in amd64/x86_64
 cmake -S . -B build -DBUILD_TESTING=OFF -DENABLE_TESTING=OFF -DVOLK_CPU_FEATURES=ON -DVOLK_USE_OWN_CPU_FEATURES=ON
+# cmake for Apple Silicon (M1/M2/etc.) macOS
+cmake -S . -B build -DBUILD_TESTING=OFF -DENABLE_TESTING=OFF -DVOLK_CPU_FEATURES=OFF
 MAKEFLAGS="-j12" cmake --build build
 # after building, do:
 # sudo zsh
