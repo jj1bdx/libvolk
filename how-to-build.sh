37e337eb406426c608aa5b612cd6a0224cf17926
Kenji Rikitake <kenji.rikitake@acm.org>
2023-04-30 12:06:28 +0900

    Update how-to-build.sh

--- how-to-build.sh
+++ how-to-build.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
+# clean up build directory 
+/bin/rm -rf build
+# Update cpu_features
+git submodule update
 # cmake script for macOS and Ubuntu in amd64/x86_64
 cmake -S . -B build -DBUILD_TESTING=OFF -DENABLE_TESTING=OFF -DVOLK_CPU_FEATURES=ON -DVOLK_USE_OWN_CPU_FEATURES=ON
 # cmake for Apple Silicon (M1/M2/etc.) macOS
 cmake -S . -B build -DBUILD_TESTING=OFF -DENABLE_TESTING=OFF -DVOLK_CPU_FEATURES=OFF
+#
+# do actual building here
+#
 MAKEFLAGS="-j12" cmake --build build
 # after building, do:
+#
 # sudo zsh
 # umask 022
 # cmake --install build
